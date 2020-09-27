d81cd435df1279f87063bacb4ae0e361093df9c8
Harsh <agarwalharshprakash@gmail.com>
2020-09-27 15:00:03 +0530

    second version of basic file

---
+++ basic.py
@@ -0,0 +1,8 @@
+print('Using git bash')
+print('Geared for hacktoberfest 2020')
+print('learning machine learning')
+print('Python program for hacktoberfest 2020')
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
